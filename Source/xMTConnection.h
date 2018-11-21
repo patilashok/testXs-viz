7173c38a99c8b41bd4a030250293e23814326fb7
ASHOK KUMAR PATIL <ashokpatil03@hotmail.com>
2018-11-21 22:14:21 +0900

    V1.0.0
    MotionRobot Version 1.0.0

---
+++ Source/xMTConnection.h
@@ -0,0 +1,187 @@
+#ifndef XMTCONNECTION_H
+#define XMTCONNECTION_H
+#include <xsensdeviceapi.h> // The Xsens device API header
+#include "conio.h"			// For non ANSI _kbhit() and _getch()
+
+#include <string>
+#include <stdexcept>
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <set>
+#include <list>
+#include <utility>
+
+#include <QThread>
+#include <QMutex>
+
+#include <xsens/xsmutex.h>
+
+#define signals Q_SIGNALS
+
+class xMTConnection : public QObject
+{
+	Q_OBJECT
+public:
+	explicit xMTConnection(QObject *parent = 0);
+
+signals:
+	void SignalToObj_mainThread();
+	void running();
+	void stopped();
+	
+
+public slots:
+	void KillMTWConnection();//stopwork()
+	void startmtw();//startwork()
+
+public:
+
+	xMTConnection();
+	~xMTConnection();
+
+	bool xmtConnect();
+
+	int findClosestUpdateRate(const XsIntArray& supportedUpdateRates, const int desiredUpdateRate);
+
+	void GetMTWdata();
+	bool UpdateMTWData(double &qRoll, double &qPitch, double &qYaw);
+	
+
+private slots:
+	void MTWData();
+	//virtual void startmtw();
+
+
+	//void signalValueUpdate(QString);
+
+private:
+	bool isRunnung,isStopped;
+
+	double mtRoll = 0;
+	double mtPitch = 0;
+	double mtYaw = 0;
+
+
+
+};
+
+//----------------------------------------------------------------------
+// Callback handler for wireless master
+//----------------------------------------------------------------------
+class WirelessMasterCallback : public XsCallback
+{
+public:
+	typedef std::set<XsDevice*> XsDeviceSet;
+
+	XsDeviceSet getWirelessMTWs() const
+	{
+		XsMutexLocker lock(m_mutex);
+		return m_connectedMTWs;
+	}
+
+protected:
+	virtual void onConnectivityChanged(XsDevice* dev, XsConnectivityState newState)
+	{
+		XsMutexLocker lock(m_mutex);
+		switch (newState)
+		{
+		case XCS_Disconnected:		/*!< Device has disconnected, only limited informational functionality is available. */
+			//std::cout << "\nEVENT: MTW Disconnected -> " << *dev << std::endl;
+			m_connectedMTWs.erase(dev);
+			break;
+		case XCS_Rejected:			/*!< Device has been rejected and is disconnected, only limited informational functionality is available. */
+			//std::cout << "\nEVENT: MTW Rejected -> " << *dev << std::endl;
+			m_connectedMTWs.erase(dev);
+			break;
+		case XCS_PluggedIn:			/*!< Device is connected through a cable. */
+			//std::cout << "\nEVENT: MTW PluggedIn -> " << *dev << std::endl;
+			m_connectedMTWs.erase(dev);
+			break;
+		case XCS_Wireless:			/*!< Device is connected wirelessly. */
+			//std::cout << "\nEVENT: MTW Connected -> " << *dev << std::endl;
+			m_connectedMTWs.insert(dev);
+			break;
+		case XCS_File:				/*!< Device is reading from a file. */
+			//std::cout << "\nEVENT: MTW File -> " << *dev << std::endl;
+			m_connectedMTWs.erase(dev);
+			break;
+		case XCS_Unknown:			/*!< Device is in an unknown state. */
+			//std::cout << "\nEVENT: MTW Unknown -> " << *dev << std::endl;
+			m_connectedMTWs.erase(dev);
+			break;
+		default:
+			//std::cout << "\nEVENT: MTW Error -> " << *dev << std::endl;
+			m_connectedMTWs.erase(dev);
+			break;
+		}
+	}
+private:
+	mutable XsMutex m_mutex;
+	XsDeviceSet m_connectedMTWs;
+};
+
+//----------------------------------------------------------------------
+// Callback handler for MTw
+// Handles onDataAvailable callbacks for MTW devices
+//----------------------------------------------------------------------
+class MtwCallback : public XsCallback
+{
+public:
+	MtwCallback(int mtwIndex, XsDevice* device)
+		:m_mtwIndex(mtwIndex)
+		, m_device(device)
+	{}
+
+	bool dataAvailable() const
+	{
+		XsMutexLocker lock(m_mutex);
+		return !m_packetBuffer.empty();
+	}
+
+	XsDataPacket const * getOldestPacket() const
+	{
+		XsMutexLocker lock(m_mutex);
+		XsDataPacket const * packet = &m_packetBuffer.front();
+		return packet;
+	}
+
+	void deleteOldestPacket()
+	{
+		XsMutexLocker lock(m_mutex);
+		m_packetBuffer.pop_front();
+	}
+
+	int getMtwIndex() const
+	{
+		return m_mtwIndex;
+	}
+
+	XsDevice const & device() const
+	{
+		assert(m_device != 0);
+		return *m_device;
+	}
+
+protected:
+	virtual void onLiveDataAvailable(XsDevice*, const XsDataPacket* packet)
+	{
+		XsMutexLocker lock(m_mutex);
+		// NOTE: Processing of packets should not be done in this thread.
+
+		m_packetBuffer.push_back(*packet);
+		if (m_packetBuffer.size() > 300)
+		{
+			std::cout << std::endl;
+			deleteOldestPacket();
+		}
+	}
+
+private:
+	mutable XsMutex m_mutex;
+	std::list<XsDataPacket> m_packetBuffer;
+	int m_mtwIndex;
+	XsDevice* m_device;
+};
+
+#endif
